33270cc4f2d8f4f14cc6839aaa02952a1ec0115f
Achiaga <alfonso.achiaga@gmail.com>
2020-03-16 20:51:17 +0100

    Add cover_page & animation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import logo from './assets/Biohazard.png';
+import CoverPage from './components/CoverPage';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
+				<CoverPage />
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<p>
-					Ready to the collapse of our society. Sit tigh bc this wanna be a hell
-					of ride. (typing)
-				</p>
-				<p>Animation character with bocadillos so seems like he talk.</p>
-				<p>Be prepare for the fun :P</p>
-				<p>Video of riots and venezuela</p>
-				<p>Factors -- Prepare</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
 			</header>
 		</div>
 	);
---
+++ src/components/CoverPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CoverPage.css';
+
+function CoverPage() {
+	return (
+		<div>
+			<div className='space'>
+				<span className='A'>A</span>poc<span className='R'>al</span>y
+				<span>p</span>sis
+			</div>
+			<div>
+				T<span className='off'>0</span>d4
+				<div id='hangEdge'>
+					<span>Y</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CoverPage;
