17518f46de207cc733614e460d16027412772b70
Achiaga <alfonso.achiaga@gmail.com>
2020-03-16 12:05:06 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/Biohazard.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p>
+					Ready to the collapse of our society. Sit tigh bc this wanna be a hell
+					of ride. (typing)
+				</p>
+				<p>Animation character with bocadillos so seems like he talk.</p>
+				<p>Be prepare for the fun :P</p>
+				<p>Video of riots and venezuela</p>
+				<p>Factors -- Prepare</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
