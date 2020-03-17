cc54fa71727a0d3ce6eae6655aa8173df22e06e3
Achiaga <alfonso.achiaga@gmail.com>
2020-03-17 18:06:39 +0100

    style front page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CoverPage from './components/CoverPage';
 import Logo from './components/Logo';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
 		<div className='app'>
 			<Logo />
 			<CoverPage />
+			<NavBar />
+			<h1>Clock</h1>
 		</div>
 	);
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from '../assets/Biohazard.png';
+import logo from '../assets/gas-mask.png';
 import './Logo.css';
 
 function Logo() {
 	return (
-		<div className='logo-container'>
-			<img src={logo} alt='logo' className='logo' />
+		<div class='logo-wrapper'>
+			<div class='logo-fountain'>
+				<img src={logo} alt='logo' className='logo' />
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class='pouce'>
+				<div class='pouce-part'></div>
+			</div>
 		</div>
 	);
 }
