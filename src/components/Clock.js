b80859749770e23d116216054989bf95004b7d67
Achiaga <alfonso.achiaga@gmail.com>
2020-03-18 19:50:37 +0100

    Clock Created

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Clock.css';
 
 export default function Clock() {
-	const [time, setTime] = useState(new Date().toLocaleString());
+	const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+	function currentTime() {
+		setTime(new Date().toLocaleTimeString());
+	}
+
+	useEffect(() => {
+		// Update the document title using the browser API
+		setInterval(() => currentTime(), 1000);
+	});
+
 	console.log(time);
 	return (
 		<div>
 			<h1>5000:10:10</h1>
-			<h1>this is the good {time}</h1>
+			<h1>{time}</h1>
 		</div>
 	);
 }
