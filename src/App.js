f87adaa80762c903ffee5159e910431f0f49cc27
Achiaga <alfonso.achiaga@gmail.com>
2020-03-20 01:09:59 +0100

    Add animation countdown

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import CoverPage from './components/CoverPage';
 import Logo from './components/Logo';
 import NavBar from './components/NavBar';
 import CountdownTimer from './components/CountdownTimer';
+import Type from './components/Type';
 import './App.css';
 
 function App() {
 	return (
 		<div className='app'>
+			<NavBar />
 			<Logo />
 			<CoverPage />
-			<NavBar />
+			<Type />
 			<CountdownTimer />
 		</div>
 	);
---
+++ src/components/Type.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Type.css';
+
+export default function Type() {
+	return (
+		<div class='css-typing'>
+			<p>Typed text 1</p>
+			<p>Typed text 2 Longer</p>
+			<p>Typed text 3</p>
+		</div>
+	);
+}
