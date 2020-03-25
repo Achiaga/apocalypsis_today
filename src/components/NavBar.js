d2b4a53faa247b1bed3414d421f018eac634345f
Achiaga <alfonso.achiaga@gmail.com>
2020-03-25 12:48:20 +0100

    transition to styled-components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,77 @@
 import React from 'react';
-import './NavBar.css';
+import styled, { keyframes } from 'styled-components';
 
-export default function Navbar() {
+const Navbar = styled.div`
+	background-color: #171717;
+	position: fixed;
+	left: 0em;
+	width: 10rem;
+	height: 100%;
+	top: 0;
+	padding: 0;
+	margin: 0;
+	color: #4d4d4e;
+	font-size: xx-small;
+	z-index: 10;
+`;
+
+const NavbarContainer = styled.div`
+	position: relative;
+	top: 27vh;
+	background-color: #171717;
+`;
+
+const bounceLetter = keyframes`
+		0% {
+			transform: scale3d(1, 1, 1);
+		}
+		30% {
+			transform: scale3d(1.25, 0.75, 1);
+		}
+		40% {
+			transform: scale3d(1.15, 0.85, 1);
+		}
+		65% {
+			transform: scale3d(0.95, 1.05, 1);
+		}
+		75% {
+			transform: scale3d(1.05, 0.95, 1);
+		}
+		100% {
+			transform: scale3d(1, 1, 1);
+		}ransform: rotate(360deg);
+	  }
+	`;
+
+const Element = styled.h1`
+	padding: 1rem;
+	margin: 0;
+	z-index: 100;
+	&:hover {
+		cursor: pointer;
+		color: cyan;
+		overflow: hidden;
+		animation: ${bounceLetter} 0.9s;
+	}
+	&:nth-child(1) {
+		color: cyan;
+	}
+	&:active {
+		color: cyan;
+	}
+`;
+
+export default function Drawer() {
 	return (
-		<div className='navbar'>
-			<div className='navbar-container'>
-				<h1 to='/'>Home</h1>
-				<h1>Survival</h1>
-				<h1>Map</h1>
-				<h1>News</h1>
-				<h1>Theories</h1>
-				<h1>Day Zero</h1>
-			</div>
-		</div>
+		<Navbar>
+			<NavbarContainer>
+				<Element>Home</Element>
+				<Element>Survival</Element>
+				<Element>Map</Element>
+				<Element>News</Element>
+				<Element>Theories</Element>
+				<Element>Day Zero</Element>
+			</NavbarContainer>
+		</Navbar>
 	);
 }
--- src/components/OctopusLogo.js
+++ src/components/OctopusLogo.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import logo from '../assets/gas-mask.png';
+import styled, { keyframes } from 'styled-components';
 import './OctopusLogo.css';
+
+const LogoWrapper = styled.div`
+	width: 150px;
+	height: 100px;
+	position: absolute;
+	top: 0;
+	margin-top: 12rem;
+	margin-left: 10rem;
+	transform: scale(2) translate(-50%, -50%);
+	z-index: 15;
+`;
+
+const colorShifter = keyframes`
+	0% {
+		box-shadow: 0 0 80px cyan, 0 0 30px darkcyan, 0 0 6px darkcyan;
+		transform: scale(0.9);
+	}
+	33% {
+		box-shadow: 0 0 80px red, 0 0 30px darkred, 0 0 6px darkred;
+		transform: scale(1.3);
+	}
+	66% {
+		box-shadow: 0 0 80px yellow, 0 0 30px yellow, 0 0 6px darkgoldenrod;
+		transform: scale(0.9);
+	}
+	99% {
+		box-shadow: 0 0 80px cyan, 0 0 30px darkcyan, 0 0 6px darkcyan;
+		transform: scale(1.3);
+	}
+	`;
+
+const MaskLogo = styled.img`
+	width: 2rem;
+	background-size: cover;
+	border-radius: 50%;
+	padding: 0;
+	box-shadow: 0 0 80px cyan, 0 0 30px darkcyan, 0 0 6px darkcyan;
+	&:hover {
+		cursor: pointer;
+		animation: ${colorShifter} 20s infinite;
+	}
+`;
+
+const LogoFountain = styled.div`
+	& > span {
+		display: block;
+		margin: 5.1rem;
+		margin-left: 4.7rem;
+		position: absolute;
+		border-left: 1px dashed lightgreen;
+		transform-origin: 50% 100%;
+		transform: translate(var(--tranlation));
+		--tranlation: -50%;
+		bottom: 4px;
+		z-index: -1;
+		width: 24px;
+		border-radius: 50%;
+	}
+`;
 
 function Logo() {
 	return (
-		<div className='logo-wrapper'>
-			<div className='logo-fountain'>
-				<img src={logo} alt='logo' className='logo' />
+		<LogoWrapper>
+			<LogoFountain className='logo-fountain'>
+				<MaskLogo src={logo} alt='logo' />
 				<span></span>
 				<span></span>
 				<span></span>
@@ -18,11 +78,8 @@ function Logo() {
 				<span></span>
 				<span></span>
 				<span></span>
-			</div>
-			<div className='pouce'>
-				<div className='pouce-part'></div>
-			</div>
-		</div>
+			</LogoFountain>
+		</LogoWrapper>
 	);
 }
 
